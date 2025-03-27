3c85df7547d6702622b0286ffe41da81bff3a4c2
Vishal-22-26 <130916964+Vishal-22-26@users.noreply.github.com>
2025-03-27 15:52:12 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),
-    react()],
+  plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'build', // Change output directory from 'dist' to 'build'
+  },
 })
+
